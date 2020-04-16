3ad8ec30111cdbc99335704011adc588c3d4aa02
mazpilik <mazpilic@gmail.com>
2020-04-16 20:18:39 +0200

    change database user

--- include/DB.php
+++ include/DB.php
@@ -7,7 +7,7 @@ require_once('prueba.php');
 class DB {
     protected static function ejecutaConsulta($sql) {
         $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-        $dsn = "mysql:host=localhost;dbname=crimebook";
+        $dsn = "mysql:host=localhost;dbname=crimebookluis";
         $usuario = 'dwes';
         $contrasena = 'abc123.';
         $dwes = new PDO($dsn, $usuario, $contrasena, $opc);
